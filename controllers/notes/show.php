a19aca0c30574ca5e903057972bd429c4a76043a
HadyElMaageny <87091270+HadyElMaageny@users.noreply.github.com>
2024-10-12 21:00:37 +0300

    Initial commit

---
+++ controllers/notes/show.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+$currentUser = 1;
+
+$note = $db->query("SELECT * FROM notes where id = :id", ['id' => $_GET['id'],])->findOrFail();
+authorize($note['user_id'] == $currentUser);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $db->query("DELETE FROM notes where id = :id", [
+        'id' => $_POST['id']
+    ]);
+
+    header('Location: /notes');
+    exit();
+}
+else {
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note,
+    ]);
+
+}
---
+++ views/notes/index.view.php
@@ -0,0 +1,22 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+
+<?php require base_path('views/partials/banner.php') ?>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <?php foreach ($notes as $note) : ?>
+                <li>
+                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                        <?= htmlspecialchars($note['body']) ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+
+            <ul class="mt-10 text-blue-500 hover:underline font:bold">
+                <a href="/notes/create">Create Note</a>
+            </ul>
+        </div>
+
+
+    </main>
+<?php require base_path('views/partials/footer.php') ?>
