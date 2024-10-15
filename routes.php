cc089f25c2ce5ea9b0e3956f4ead60917bd2f7de
HadyElMaageny <87091270+HadyElMaageny@users.noreply.github.com>
2024-10-15 15:31:38 +0300

    Creating Middleware

--- routes.php
+++ routes.php
@@ -13,7 +13,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->get('/note/edit', 'controllers/notes/edit.php');
@@ -24,7 +24,7 @@ $router->delete('/note', 'controllers/notes/destroy.php');
 
 $router->post('/notes', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 
 $router->get('/employees', 'controllers/employees/index.php');
